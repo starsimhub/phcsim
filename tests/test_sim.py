cf7abc118aff9ccc5bc94de2d10e01864eed6fb2
Cliff Kerr <questionably@gmail.com>
2025-03-02 21:34:58 -0800

    add test sim

---
+++ tests/test_sim.py
@@ -0,0 +1,27 @@
+"""
+Test full sim
+"""
+
+import sciris as sc
+import phcsim as phc
+
+do_plot = False
+
+
+def test_basic_sim(do_plot=do_plot):
+    sc.heading('Testing basic sim')
+    sim = phc.Sim()
+    sim.init()
+    sim.run()
+    return sim
+
+
+if __name__ == '__main__':
+
+    do_plot = True
+
+    T = sc.timer()
+
+    sim = test_basic_sim(do_plot=do_plot)
+
+    T.toc()
